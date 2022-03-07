a7667d9b3faa6805625d3fab2ac5974608e4ff46
ComradeVanti <kingvanti650@gmail.com>
2022-03-07 10:52:03 +0100

    Add test

---
+++ Res.Tests/GeneralTests.cs
@@ -0,0 +1,12 @@
+﻿using FsCheck.Xunit;
+
+namespace ComradeVanti.CSharpTools;
+
+public class GeneralTests
+{
+
+    [Property]
+    public bool Results_Can_Not_Be_Ok_And_Fail(Res<int, string> res) =>
+        res.IsOk() != res.IsFail();
+
+}

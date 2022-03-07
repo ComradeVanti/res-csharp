75b39a259e74fb000ef08b4e588610fcf7b88cf6
ComradeVanti <kingvanti650@gmail.com>
2022-03-07 10:47:13 +0100

    Add logic to generate results for testing

---
+++ Res.Tests/ResGen.cs
@@ -0,0 +1,20 @@
+﻿using FsCheck;
+
+namespace ComradeVanti.CSharpTools;
+
+public static class ResGen
+{
+
+    private static Gen<Res<TOk, TFail>> GenOk<TOk, TFail>() =>
+        Arb.Generate<TOk>().Select(Res.Ok<TOk, TFail>);
+
+    private static Gen<Res<TOk, TFail>> GenFail<TOk, TFail>() =>
+        Arb.Generate<TFail>().Select(Res.Fail<TOk, TFail>);
+
+    private static Gen<Res<TOk, TFail>> GenRes<TOk, TFail>() =>
+        Gen.OneOf(GenOk<TOk, TFail>(), GenFail<TOk, TFail>());
+
+    public static Arbitrary<Res<int, string>> TestRes() =>
+        Arb.From(GenRes<int, string>());
+
+}

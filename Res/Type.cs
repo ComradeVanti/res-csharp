e1f7a8693feb915611539285a940d7cc6c38c300
ComradeVanti <kingvanti650@gmail.com>
2022-03-07 10:18:40 +0100

    Add res type

---
+++ Res/Type.cs
@@ -0,0 +1,36 @@
+﻿namespace ComradeVanti.CSharpTools
+{
+
+    /// <summary>
+    ///     A result of an operation which may either result in a value or an error
+    /// </summary>
+    /// <typeparam name="TOk">The type of the value if the result is ok</typeparam>
+    /// <typeparam name="TFail">The type of the error if the results is a failure</typeparam>
+    public abstract class Res<TOk, TFail>
+    {
+
+        internal sealed class Ok : Res<TOk, TFail>
+        {
+
+            public TOk Value { get; }
+
+
+            public Ok(TOk value) =>
+                Value = value;
+
+        }
+
+        internal sealed class Fail : Res<TOk, TFail>
+        {
+
+            public TFail Error { get; }
+
+
+            public Fail(TFail error) =>
+                Error = error;
+
+        }
+
+    }
+
+}

6e25cd5db560b8a54404cf1fe7f97b76666da8d5
ComradeVanti <kingvanti650@gmail.com>
2022-03-07 10:43:36 +0100

    Add logic to instantiate failed results

--- Res.Tests/InstantiationTests.cs
+++ Res.Tests/InstantiationTests.cs
@@ -8,5 +8,9 @@ public class InstantiationTests
     [Property]
     public bool Results_Created_With_Ok_Are_Always_Ok(int i) =>
         Res.Ok<int, string>(i).IsOk();
+    
+    [Property]
+    public bool Results_Created_With_Fail_Are_Always_Fail(string e) =>
+        Res.Fail<int, string>(e).IsFail();
 
 }
--- Res/Res.cs
+++ Res/Res.cs
@@ -17,6 +17,16 @@
         public static Res<TOk, TFail> Ok<TOk, TFail>(TOk value) =>
             new Res<TOk, TFail>.Ok(value);
 
+        /// <summary>
+        ///     Creates a failed result
+        /// </summary>
+        /// <param name="error">The error to be stored in the result</param>
+        /// <typeparam name="TOk">The type of the value if the result is ok</typeparam>
+        /// <typeparam name="TFail">The type of the error if the results is a failure</typeparam>
+        /// <returns>The result</returns>
+        public static Res<TOk, TFail> Fail<TOk, TFail>(TFail error) =>
+            new Res<TOk, TFail>.Fail(error);
+
     }
 
 }
